ad569fcb61f5ea1367b4c4e9500fff06174fc639
akshat verma <akshatv092@gmail.com>
2023-11-22 12:13:49 +0530

    simple calculater

---
+++ index.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'AC') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'DEL') {
+              buttonText.innerHTML = buttonText.innerHTML.substr(0,buttonText.innerHTML.length-1)
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
